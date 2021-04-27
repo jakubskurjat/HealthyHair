cc6792227475cc2cf2ef1198a5976b9dae979edb
Jakub Skurjat <jakubskurjat99@gmail.com>
2021-04-27 22:40:53 +0200

    Created Created adding a user to the database, logging in and user registration

--- app/src/main/java/com/example/healthyhair/MainActivity.java
+++ app/src/main/java/com/example/healthyhair/MainActivity.java
@@ -2,20 +2,36 @@ package com.example.healthyhair;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
+    private EditText tvEmail, tvPassword;
     private TextView createAccount;
+    private Button btnLogin;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        tvEmail = findViewById(R.id.inEmail);
+        tvPassword = findViewById(R.id.inPassword);
+        btnLogin = findViewById(R.id.btnLogIn);
         createAccount = findViewById(R.id.inCreateAccount);
+
+        btnLogin.setOnClickListener(v -> loginUserAccount());
 
         createAccount.setOnClickListener(view -> {
             Intent intent
@@ -23,5 +39,45 @@ public class MainActivity extends AppCompatActivity {
                     SignUpActivity.class);
             startActivity(intent);
         });
+    }
+
+    private void loginUserAccount() {
+
+        String email, password;
+        email = tvEmail.getText().toString();
+        password = tvPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(),
+                    "Please enter email!",
+                    Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                    "Please enter password!",
+                    Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Login successful!",
+                                        Toast.LENGTH_LONG)
+                                        .show();
+                            } else {
+
+                                Toast.makeText(getApplicationContext(),
+                                        "Login failed!",
+                                        Toast.LENGTH_LONG)
+                                        .show();
+                            }
+                        });
     }
 }
--- app/src/main/java/com/example/healthyhair/SignUpActivity.java
+++ app/src/main/java/com/example/healthyhair/SignUpActivity.java
@@ -1,14 +1,77 @@
 package com.example.healthyhair;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText tvEmail, tvPassword;
+    private Button btnSignup;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        tvEmail = findViewById(R.id.upEmail);
+        tvPassword = findViewById(R.id.upPassword);
+        btnSignup = findViewById(R.id.btnSignUp);
+
+        btnSignup.setOnClickListener(v -> registerNewUser());
+    }
+
+    private void registerNewUser() {
+
+        String email, password;
+        email = tvEmail.getText().toString();
+        password = tvPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(),
+                    "Please enter email!",
+                    Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                    "Please enter password!",
+                    Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getApplicationContext(),
+                                "Registration successful!",
+                                Toast.LENGTH_LONG)
+                                .show();
+
+                        Intent intent
+                                = new Intent(SignUpActivity.this,
+                                MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "Registration failed!"
+                                        + " User already exists!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
     }
 }
