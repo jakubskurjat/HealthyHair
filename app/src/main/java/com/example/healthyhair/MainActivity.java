b6cb87cd6495ee2a505c14640f62cabac0dc0f50
Jakub Skurjat <jakubskurjat99@gmail.com>
2021-04-27 22:02:32 +0200

    Communication has been established between the login and registration layout

--- app/src/main/java/com/example/healthyhair/MainActivity.java
+++ app/src/main/java/com/example/healthyhair/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.healthyhair;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextView createAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_main);
+
+        createAccount = findViewById(R.id.inCreateAccount);
+
+        createAccount.setOnClickListener(view -> {
+            Intent intent
+                    = new Intent(MainActivity.this,
+                    SignUpActivity.class);
+            startActivity(intent);
+        });
     }
 }
