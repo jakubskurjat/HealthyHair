241accd6a5d71301415bf7b47d96a38310300e09
Jakub Skurjat <jakubskurjat99@gmail.com>
2021-05-04 14:30:15 +0200

    Created DailyPEHSurveyQuestions

---
+++ app/src/main/java/com/example/healthyhair/DailyPEHSurveyQuestions.java
@@ -0,0 +1,20 @@
+package com.example.healthyhair;
+
+public class DailyPEHSurveyQuestions {
+
+    String [] question = {
+            "Is your hair overburdened?",
+            "Is your hair spinning?",
+            "Is your hair dull?",
+            "Is your hair dry?",
+            "Does your hair break?",
+            "Does your hair get static?",
+            "Is your hair frizzy?",
+            "Did you notice the straightening of the twist?",
+            "Does your hair get greasy quickly?"
+    };
+
+    public String getQuestion(int a) {
+        return question[a];
+    }
+}
