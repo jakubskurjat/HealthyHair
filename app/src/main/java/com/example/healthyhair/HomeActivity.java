082989d5c40668badf28739503b0cd670b97d68d
Jakub Skurjat <jakubskurjat99@gmail.com>
2021-05-04 11:47:55 +0200

    Implemented layout for Home Activity

--- app/src/main/java/com/example/healthyhair/HomeActivity.java
+++ app/src/main/java/com/example/healthyhair/HomeActivity.java
@@ -11,11 +11,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.porosityNavBar);
         Menu menu = bottomNavigationView.getMenu();
